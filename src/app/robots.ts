06f3b98c698e62874fdd360a68c6fc794295866e
rafaelviefe <rafaelviefe@gmail.com>
2026-05-10 15:01:19 -0300

    Update metadata URLs for consistency and improved SEO

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://calculadora.rafaelviefe.app/sitemap.xml',
+    sitemap: 'https://rafaelviefe.app/sitemap.xml',
   }
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,8 +3,8 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://calculadora.rafaelviefe.app',
-      lastModified: new Date('2024-05-10'),
+      url: 'https://rafaelviefe.app',
+      lastModified: new Date('2026-05-10'),
       changeFrequency: 'monthly',
       priority: 1,
     },
